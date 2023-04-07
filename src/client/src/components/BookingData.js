c68914b363abf46efe98e49581084d342544b190
unknown <nagleeesahil@gmail.com>
2023-04-07 15:34:53 +0530

    booking details reversed

--- src/client/src/components/BookingData.js
+++ src/client/src/components/BookingData.js
@@ -21,45 +21,46 @@ const BookingData = () => {
   });
 
   return (
-    <div>
-      <div className='flex'>
+    <>
+      <div className='flex '>
         <h3 className='w-8/12 mb-3'>All Bookings</h3>
         <p className='self-end justify-self-end'>
           Total Bookings : {bookingInfo.length}
         </p>
       </div>
+      <div className='flex flex-col-reverse'>
+        {/*-------- If no bookings ---------*/}
+        {bookingInfo.length == 0 ? (
+          <div className='text-center'>no previous booking found</div>
+        ) : (
+          // -------If bookings -------------
 
-      {/*-------- If no bookings ---------*/}
-      {bookingInfo.length == 0 ? (
-        <div className='text-center'>no previous booking found</div>
-      ) : (
-        // -------If bookings -------------
+          bookingInfo.map((info, index) => (
+            <div className='mb-4' key={index}>
+              <div className='border-2 rounded-2xl p-4 mb-7 bg-white'>
+                <h4>Last Booking Details</h4>
+                <p className='font-bold mb-1'>Seats:</p>
+                {Object.entries(info.seats).map(([seat, count]) => (
+                  <p key={seat} className='mb-1'>
+                    <span className='font-bold'>{seat}: </span>
+                    {count}
+                  </p>
+                ))}
 
-        bookingInfo.map((info, index) => (
-          <div className='mb-4' key={index}>
-            <div className='border-2 rounded-2xl p-4 mb-7 bg-white'>
-              <h4>Last Booking Details</h4>
-              <p className='font-bold mb-1'>Seats:</p>
-              {Object.entries(info.seats).map(([seat, count]) => (
-                <p key={seat} className='mb-1'>
-                  <span className='font-bold'>{seat}: </span>
-                  {count}
+                <p className=' mb-1'>
+                  <span className='font-bold'>Slot: </span>
+                  {info.slot}
                 </p>
-              ))}
-
-              <p className=' mb-1'>
-                <span className='font-bold'>Slot: </span>
-                {info.slot}
-              </p>
-              <p className=' mb-1'>
-                <span className='font-bold'>Movie: </span>
-                {info.movie}
-              </p>
+                <p className=' mb-1'>
+                  <span className='font-bold'>Movie: </span>
+                  {info.movie}
+                </p>
+              </div>
             </div>
-          </div>
-        ))
-      )}
-    </div>
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
